55f70c191ccb37ca869dcca8cee3cef3b9f998b9
Ammar Mahmoud <69160488+ammar-hup@users.noreply.github.com>
2022-09-27 17:40:06 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,54 +1,45 @@
-// Global Variables
-
 const ul = document.querySelector("#navbar__list");
 const sections = document.querySelectorAll("section");
-const fragment = document.createDocumentFragment();
+const fragment_file = document.createDocumentFragment();
 
-// Scroll to section on link click
-
-function buildNavigationMenu() {
-  sections.forEach(function (section) {
-    const sectionId = section.id;
-    const sectionTitle = section.getAttribute("data-nav");
-    const li = document.createElement("li");
-    const link = document.createElement("a");
-    link.setAttribute("href", "#" + "sectionId");
-    // link.href = '#${sectionId}'
-    link.setAttribute("class", "menu__link");
-    link.textContent = sectionTitle;
-    link.addEventListener("click", function (e) {
-      e.preventDefault();
-      section.scrollIntoView({
-        behavior: "smooth",
-      });
+let i = 1;
+for (const section of sections) {
+  const sectionId = section.id;
+  const li = document.createElement("li");
+  const link = document.createElement("a");
+  link.href = "#${sectionId}";
+  link.classList = "menu__link";
+  link.textContent = "section " + i;
+  link.addEventListener("click", function (evannt) {
+    evannt.preventDefault();
+    section.scrollIntoView({
+      behavior: "smooth",
     });
-    li.appendChild(link);
-    li.style.cssText =
-      "color: black; opacity : 0.9; font-family: Arial, Helvetica, sans-serif;align-content:center ; text-transform: capitalize; font-style: italic ;";
-    document.getElementById("navbar__list").style.cssText =
-      "text-align : center;";
-    fragment.appendChild(li);
   });
-  ul.appendChild(fragment);
+  li.appendChild(link);
+  li.style.cssText =
+    "color: black; opacity : 0.9; font-family: Arial, Helvetica, sans-serif;align-content:center ; text-transform: capitalize; font-style: italic ;";
+  document.getElementById("navbar__list").style.cssText =
+    "text-align : center;";
+  fragment_file.appendChild(li);
+  i++;
 }
-window.addEventListener("load", buildNavigationMenu);
+ul.appendChild(fragment_file);
 
-// make a shadow for section background
-
-function callback(entries) {
-  const actvLink = document.querySelector(`a[href="#${entries[0].target.id}"]`);
-  if (entries[0].isIntersecting) {
-    entries[0].target.classList.add("your-active-class");
+function callback(e) {
+  const actvLink = document.querySelector(`a[href="#${e[0].target.id}"]`);
+  if (e[0].isIntersecting) {
+    e[0].target.classList.add("your-active-class");
     actvLink.classList.add("active-lnk");
   } else {
-    entries[0].target.classList.remove("your-active-class");
+    e[0].target.classList.remove("your-active-class");
     actvLink.classList.remove("active-lnk");
   }
 }
 const choice = {
   root: null,
   rootMargin: "0px",
-  threshold: 0.68,
+  threshold: 0.5,
 };
 const tracker = new IntersectionObserver(callback, choice);
 
